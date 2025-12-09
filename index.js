9a6f1d24a44799bbbc72320353d673059c19a225
Itashaf <tashafmahmood00@gmail.com>
2025-12-09 16:51:59 +0530

    changes

--- index.js
+++ index.js
@@ -141,5 +141,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server listening`);
+  console.log(`Server listening on port: ${process.env.PORT}`);
 });
